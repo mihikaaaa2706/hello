946dea165d7a07024d2254e2206afc55a12c3870
mihikaaaa2706 <mihikagupta2776@gmail.com>
2026-04-13 10:44:14 +0530

    Add files via upload

---
+++ HelloWorld6.java
@@ -0,0 +1,19 @@
+public class HelloWorld6 {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            String result = "";
+
+            for (String name : args) {
+                result += name + ", ";
+            }
+
+            // Remove last ", "
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println("Hello, " + result + "!");
+        }
+    }
+}
